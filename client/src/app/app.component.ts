4566077d21f600208d531922a06f58b7b708accc
Yunchov98 <georgiiunchov@gmail.com>
2025-10-06 20:18:31 +0300

    Update app configs

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { initializeApp } from '@angular/fire/app';
+import { firebaseConfig } from './core/firebase/firebase.config';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
+
+  constructor() {
+    initializeApp(firebaseConfig);
+  }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { firebaseConfig } from './core/firebase/firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  ],
 };
