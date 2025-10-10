f4883f43f3f00fe1e4125e54b3a9791688b093b8
Yunchov98 <georgiiunchov@gmail.com>
2025-10-10 20:02:11 +0300

    Update merge and config files

--- client/src/app/core/firebase/firebase.config.ts
+++ client/src/app/core/firebase/firebase.config.ts
@@ -1,11 +1,9 @@
-import { firebaseSecret } from '../../../environments/firebase.secret';
-
 export const firebaseConfig = {
-  apiKey: firebaseSecret.apiKey,
-  authDomain: firebaseSecret.authDomain,
-  databaseURL: firebaseSecret.databaseURL,
-  projectId: firebaseSecret.projectId,
-  storageBucket: firebaseSecret.storageBucket,
-  messagingSenderId: firebaseSecret.messagingSenderId,
-  appId: firebaseSecret.appId,
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  databaseURL: process.env['FIREBASE_DATABASE_URL'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
 };
